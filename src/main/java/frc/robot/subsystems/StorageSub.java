1642f73ddfcedcd9a16aebe414f6d84fb4d24034
chickensalt <nsdj.sharma@gmail.com>
2022-01-28 16:32:42 +1100

    remove unused import

--- src/main/java/frc/robot/subsystems/StorageSub.java
+++ src/main/java/frc/robot/subsystems/StorageSub.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.structures.TalonSRXMotorController;
