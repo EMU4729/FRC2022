5166bdcfb84e2234f04cd46c3004f24c64876c2e
chickensalt <nsdj.sharma@gmail.com>
2022-01-31 18:35:59 +1100

    add preliminary ballstop subsystem

---
+++ src/main/java/frc/robot/subsystems/BallStopSub.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class BallStopSub extends SubsystemBase {
+  private final WPI_TalonSRX motor = new WPI_TalonSRX(Constants.ballStopMotorPort);
+
+  @Override
+  public void periodic() {
+  }
+
+  /**
+   * Sets the speed of the ball stop motor
+   * 
+   * @param speed Speed between -1 and 1
+   */
+  public void set(double speed) {
+    motor.set(speed);
+  }
+
+}
