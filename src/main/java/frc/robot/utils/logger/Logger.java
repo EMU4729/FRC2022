15f4fe01193716039a9657ee637fa009f618b6bd
chickensalt <nsdj.sharma@gmail.com>
2022-02-23 19:16:22 +1100

    improve the logger

---
+++ src/main/java/frc/robot/utils/logger/Logger.java
@@ -0,0 +1,84 @@
+package frc.robot.utils.logger;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Date;
+import java.util.Calendar;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import frc.robot.Constants;
+
+public class Logger {
+  private static Optional<Logger> instance = Optional.empty();
+  private ArrayList<LogLine> logCache = new ArrayList<LogLine>();
+  private final String logFileName;
+  private final Constants consts = Constants.getInstance();
+  private boolean fileCreationFailed = false;
+
+  private Logger() {
+    Date date = Calendar.getInstance().getTime();
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_hh-mm");
+    String strDate = dateFormat.format(date);
+    String tempLogFileName = consts.FilePath + strDate + ".txt";
+    File logFile = new File(tempLogFileName);
+
+    try {
+      for (int i = 1; !logFile.createNewFile(); i++) {
+        if (i > 10) {
+          fileCreationFailed = true;
+          System.out.println("error : Log file creation failed : time out");
+          break;
+        }
+        tempLogFileName = consts.FilePath + strDate + "_(" + i + ")" + ".txt";
+        logFile = new File(tempLogFileName);
+      }
+    } catch (IOException e) {
+      System.out.println("error : Log file creation failed : " + e.toString());
+    }
+
+    logFileName = tempLogFileName;
+  }
+
+  public static Logger getInstance() {
+    if (!instance.isPresent()) {
+      instance = Optional.of(new Logger());
+    }
+    return instance.get();
+  }
+
+  public static void info(String content) {
+    Logger logger = getInstance();
+    logger.logCache.add(new LogLine(content, LogLevel.INFO));
+  }
+
+  public static void warn(String content) {
+    Logger logger = getInstance();
+    logger.logCache.add(new LogLine(content, LogLevel.WARN));
+  }
+
+  public static void error(String content) {
+    Logger logger = getInstance();
+    logger.logCache.add(new LogLine(content, LogLevel.ERROR));
+  }
+
+  public void save() {
+    if (logCache.isEmpty() || fileCreationFailed) {
+      return;
+    }
+    try {
+      FileWriter logWriter = new FileWriter(logFileName, true);
+      while (!logCache.isEmpty()) {
+        logWriter.write(logCache.get(0).toString());
+        logWriter.write(System.getProperty("line.separator"));
+        logCache.remove(0);
+      }
+      logWriter.close();
+    } catch (IOException e) {
+      System.out.println("error : write to log file failed : " + e.toString());
+    }
+  }
+}
