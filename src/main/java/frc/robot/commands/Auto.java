a36af59c3cc1dae57a16ed1152ab3ff6158743af
chickensalt <nsdj.sharma@gmail.com>
2022-03-02 20:07:47 +1100

    move stuff around

--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -9,11 +9,11 @@ import java.util.List;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.structures.AutoCommand;
 import frc.robot.subsystems.BallStopSub;
 import frc.robot.subsystems.DriveSub;
 import frc.robot.subsystems.IntakeSub;
 import frc.robot.subsystems.NavigationSub;
+import frc.robot.utils.AutoCommand;
 
 public class Auto extends CommandBase {
   private final Constants constants = Constants.getInstance();
