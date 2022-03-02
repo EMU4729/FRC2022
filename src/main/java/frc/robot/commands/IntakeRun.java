b26fe99da82ae318b774883025fc5baa0c3d1982
roterex <joel.roterex@gmail.com>
2022-03-02 20:07:55 +1100

    started on intake group command logic

--- src/main/java/frc/robot/commands/IntakeRun.java
+++ src/main/java/frc/robot/commands/IntakeRun.java
@@ -1,20 +1,73 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.IntakeSub;
+import frc.robot.subsystems.StorageSub;
 
 public class IntakeRun extends CommandBase {
   private final IntakeSub intake;
+  private final StorageSub storage;
 
-  public IntakeRun(IntakeSub intake) {
+  private int ballCount = 0;
+  private boolean finished = false;
+
+  public IntakeRun(IntakeSub intake, StorageSub storage) {
     this.intake = intake;
+    this.storage = storage;
     addRequirements(intake);
+    addRequirements(storage);
   }
 
   @Override
   public void initialize() {
-    // TODO: Implement this properly
-    intake.setSpinSpeed(0.5);
+    if(storage.getTopColor() == Color.kBlue || storage.getTopColor() == Color.kRed){
+      if(storage.getTopColor() == Color.kBlue || storage.getTopColor() == Color.kRed){
+        ballCount = 2;
+      } else{
+        ballCount = 1;
+      }
+    }
+  }
+
+  @Override
+  public void execute(){
+    new ConditionalCommand(
+      new ConditionalCommand(
+        new StartEndCommand(
+          () -> storage.setConveyorSpeed(0.5), 
+          () -> storage.setConveyorSpeed(0), 
+          storage)
+            .withTimeout(5)
+            .until(storage.getTopColor() == DriverStation.getAlliance()), 
+        storage.getTopColor() == DriverStation.getAlliance()),
+      new ConditionalCommand(
+        new ParallelCommandGroup(
+          new storage.setConveyorSpeed(10),
+          new SequentialCommandGroup(
+            new WaitUntilCommand(storage.limitSwitch),
+            ConditionalCommand(
+              //while top sensor not triggered
+              //while bottom sensor not triggered
+              ballCount == 0),
+            new InstantCommand(storage.setConveyorSpeed(0), storage),
+            ballCount++
+          )),
+        new InstantCommand(intake.setSpinSpeed(0), intake),
+        ballCount >= 2), 
+      ballCount >= 1);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return finished;
   }
 
   @Override
